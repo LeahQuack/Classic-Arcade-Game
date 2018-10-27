aa0274bea6ff1b1b6cdeb65421ba917741998069
Leah Quackenbush <31069314+LeahQuack@users.noreply.github.com>
2018-10-27 10:39:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,49 +1,46 @@
-// Soreboard
+// EXTRA TEST
 var score = 0; // define score of the game
-var level = 0;// game level
+var level = 0; // game level
 
 
 
 // Enemies our player must avoid
-let Enemy = function(x,y, speed) {
+let Enemy = function(x, y, speed) {
   // x and y: x is horizonal, y is vertical
-    this.x = x;
-    this.y = y + 55;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-    this.step = 101;
-    this.boundary = this.step * 5;
-    this.resetPos = -this.step;
+  this.x = x;
+  this.y = y + 55;
+  this.speed = speed;
+  this.sprite = 'images/enemy-bug.png';
+  this.step = 101;
+  this.boundary = this.step * 5;
+  this.resetPos = -this.step;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 
-    // If enemy is not passed boundary
-    if (this.x < this.boundary) {
-      // Move forward
-      this.x += this.speed * dt;
-    }
-
-    else {
-      // Reset to start
-      this.x = this.resetPos;
-    }
+  // If enemy is not passed boundary
+  if (this.x < this.boundary) {
+    // Move forward
+    this.x += this.speed * dt;
+  } else {
+    // Reset to start
+    this.x = this.resetPos;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
 // Hero class
 class Hero {
   constructor() {
@@ -51,7 +48,7 @@ class Hero {
     this.step = 101;
     this.jump = 83;
     this.startX = this.step * 2;
-    this.startY = (this.jump * 4 ) + 55;
+    this.startY = (this.jump * 4) + 55;
     this.x = this.startX;
     this.y = this.startY;
     this.victory = false;
@@ -60,15 +57,16 @@ class Hero {
   // Draw hero sprite on current x and y coord Position
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    renderScoreBoard();
   }
 
   /**
-  * Update hero's x and y property according to the input
-  *
-  * @param {string} input - direction to travel!
-  */
+   * Update hero's x and y property according to the input
+   *
+   * @param {string} input - direction to travel!
+   */
   handleInput(input) {
-    switch(input) {
+    switch (input) {
       case 'left':
         if (this.x > 0) {
           this.x -= this.step;
@@ -89,28 +87,30 @@ class Hero {
           this.y += this.jump;
         };
         break;
-      }
+    }
   }
 
   update() {
     // Check collision
     for (let enemy of allEnemies) {
       // Did player x y collide with enemy x y?
-      if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
+      if (this.y === enemy.y && (enemy.x + enemy.step / 2 > this.x && enemy.x < this.x + this.step / 2)) {
         this.reset();
       }
     }
     // Check for a winner
-      // Did player x and y reach final tile??
-      if(this.y <= 0) {
-        this.reset();
-        level = level + 1;
-        score = score + 10;
-      }
+    // Did player x and y reach final tile??
+    // Level up & increase score for making it across!
+    if (this.y <= 0) {
+      this.reset();
+      level = level + 1;
+      score = score + 10;
+    }
     if (score >= 50) {
+//const bug1 = new Enemy(-101, 0, 150);
     }
   }
-  reset () {
+  reset() {
     this.y = this.startY;
     this.x = this.startX;
   }
@@ -121,10 +121,10 @@ class Hero {
 // Place the player object in a variable called player
 const player = new Hero();
 const bug1 = new Enemy(-101, 0, 350);
-const bug2 = new Enemy((-101*2.5), 83, 300);
+const bug2 = new Enemy((-101 * 2.5), 83, 300);
 const bug3 = new Enemy(-101, 166, 450);
 const allEnemies = [];
-allEnemies.push(bug1,bug2,bug3);
+allEnemies.push(bug1, bug2, bug3);
 
 //SCOREBOARD WOO!
 renderScoreBoard = function() {
@@ -143,12 +143,12 @@ renderScoreBoard = function() {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
